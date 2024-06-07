8440fcfa55601aa39a66b3df575c28ccbd4dae62
datalogics-jacksonm <jacksonm@datalogics.com>
2024-06-07 15:05:14 -0500

    Add types for the Documentation overhaul
    Adds three types:
    
    - Documentation (single type)
    - Documentation Topic (collection type)
    - Documentation Section (collection type)
    
    Documentation should be the catch-all for the header of the
    /documentation page, and is intended to hold the SEO metadata.
    
    Documentation Topics are intended to be the parent sections (Cloud API,
    Self-Hosted API, etc.)
    
    Documentation Sections are for any other section under an existing
    Documentation Topic or Documentation Section.

---
+++ src/api/documentation-topic/controllers/documentation-topic.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * documentation-topic controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::documentation-topic.documentation-topic');
---
+++ src/api/documentation-topic/services/documentation-topic.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * documentation-topic service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::documentation-topic.documentation-topic');
