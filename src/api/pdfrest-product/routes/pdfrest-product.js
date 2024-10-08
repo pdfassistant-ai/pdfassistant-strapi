392163317928f9ee907d6cf316fc9caaaa1d91f1
datalogics-dliang <dliang@datalogics.com>
2024-10-08 11:35:05 -0500

    Revert "Add pdfRest products type, component changes"
    This reverts commit 6999dadde59e232631ca14c8f841636f8df4c60e.

--- src/api/pdfrest-product/routes/pdfrest-product.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * pdfrest-product router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::pdfrest-product.pdfrest-product');
