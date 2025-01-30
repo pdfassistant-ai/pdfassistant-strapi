0c35256013c3e7c78e545ee0b687af03daf6af8a
datalogics-dliang <dliang@datalogics.com>
2025-01-30 10:51:15 -0600

    Add plugins hub overview single type
    * Also adds a Plugins Group component

---
+++ src/api/pdfassistant-plugins-overview/controllers/pdfassistant-plugins-overview.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pdfassistant-plugins-overview controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::pdfassistant-plugins-overview.pdfassistant-plugins-overview');
