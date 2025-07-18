eddb4ebeb20c9154173e2a57cdcce9bfc279bd97
datalogics-jacksonm <jacksonm@datalogics.com>
2025-07-18 15:13:38 -0500

    Add links to the FAQ sections

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,31 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ToolParameter extends Schema.Component {
+  collectionName: 'components_tool_parameters';
+  info: {
+    displayName: 'Parameter';
+    icon: 'apps';
+    description: '';
+  };
+  attributes: {
+    name: Attribute.String;
+    description: Attribute.RichText;
+    highlighted_parameter: Attribute.Boolean & Attribute.DefaultTo<false>;
+  };
+}
+
+export interface ToolCard extends Schema.Component {
+  collectionName: 'components_tool_cards';
+  info: {
+    displayName: 'Card';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    body: Attribute.RichText;
+  };
+}
+
 export interface SharedString extends Schema.Component {
   collectionName: 'components_shared_strings';
   info: {
@@ -167,41 +193,6 @@ export interface SharedCard extends Schema.Component {
   };
 }
 
-export interface PricingFeature extends Schema.Component {
-  collectionName: 'components_pricing_features';
-  info: {
-    displayName: 'Feature';
-    icon: 'check';
-    description: '';
-  };
-  attributes: {
-    detail: Attribute.String;
-  };
-}
-
-export interface PricingCard extends Schema.Component {
-  collectionName: 'components_pricing_cards';
-  info: {
-    displayName: 'card';
-    icon: 'priceTag';
-    description: '';
-  };
-  attributes: {
-    title: Attribute.String;
-    description: Attribute.Text;
-    features: Attribute.Component<'pricing.feature', true>;
-    price: Attribute.String;
-    discounted_price: Attribute.String;
-    user_state: Attribute.JSON;
-    discount_text: Attribute.String;
-    highlight: Attribute.Boolean;
-    badge_text: Attribute.String;
-    cycle: Attribute.String;
-    stripe_data: Attribute.JSON;
-    price_data: Attribute.JSON;
-  };
-}
-
 export interface ProductToolGroup extends Schema.Component {
   collectionName: 'components_product_tool_groups';
   info: {
@@ -258,90 +249,6 @@ export interface ProductPdfassistantProductSection extends Schema.Component {
     links: Attribute.Component<'shared.link', true>;
     icon: Attribute.String;
     card_style: Attribute.Enumeration<['landing']>;
-  };
-}
-
-export interface HeaderLink extends Schema.Component {
-  collectionName: 'components_header_links';
-  info: {
-    displayName: 'Link';
-    icon: 'link';
-    description: '';
-  };
-  attributes: {
-    label: Attribute.String;
-    description: Attribute.String;
-    to: Attribute.String;
-    icon: Attribute.String;
-    external: Attribute.Boolean;
-    target: Attribute.Enumeration<['_blank']>;
-    children: Attribute.JSON;
-  };
-}
-
-export interface FaqQuestion extends Schema.Component {
-  collectionName: 'components_faq_questions';
-  info: {
-    displayName: 'Question';
-    icon: 'question';
-  };
-  attributes: {
-    title: Attribute.String;
-    body: Attribute.RichText;
-  };
-}
-
-export interface FaqFaqSection extends Schema.Component {
-  collectionName: 'components_faq_faq_sections';
-  info: {
-    displayName: 'FAQ Section';
-    icon: 'question';
-    description: '';
-  };
-  attributes: {
-    title: Attribute.String;
-    questions: Attribute.Component<'faq.question', true>;
-  };
-}
-
-export interface ToolParameter extends Schema.Component {
-  collectionName: 'components_tool_parameters';
-  info: {
-    displayName: 'Parameter';
-    icon: 'apps';
-    description: '';
-  };
-  attributes: {
-    name: Attribute.String;
-    description: Attribute.RichText;
-    highlighted_parameter: Attribute.Boolean & Attribute.DefaultTo<false>;
-  };
-}
-
-export interface ToolCard extends Schema.Component {
-  collectionName: 'components_tool_cards';
-  info: {
-    displayName: 'Card';
-    description: '';
-  };
-  attributes: {
-    title: Attribute.String;
-    body: Attribute.RichText;
-  };
-}
-
-export interface DocumentationDocSection extends Schema.Component {
-  collectionName: 'components_documentation_doc_sections';
-  info: {
-    displayName: 'Doc Section';
-    icon: 'file';
-  };
-  attributes: {
-    title: Attribute.String;
-    description: Attribute.RichText;
-    text_content: Attribute.RichText;
-    tabs: Attribute.Component<'tool.parameter', true>;
-    links: Attribute.Component<'header.link', true>;
   };
 }
 
@@ -434,9 +341,104 @@ export interface PricingSectionCta extends Schema.Component {
   };
 }
 
+export interface PricingFeature extends Schema.Component {
+  collectionName: 'components_pricing_features';
+  info: {
+    displayName: 'Feature';
+    icon: 'check';
+    description: '';
+  };
+  attributes: {
+    detail: Attribute.String;
+  };
+}
+
+export interface PricingCard extends Schema.Component {
+  collectionName: 'components_pricing_cards';
+  info: {
+    displayName: 'card';
+    icon: 'priceTag';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.Text;
+    features: Attribute.Component<'pricing.feature', true>;
+    price: Attribute.String;
+    discounted_price: Attribute.String;
+    user_state: Attribute.JSON;
+    discount_text: Attribute.String;
+    highlight: Attribute.Boolean;
+    badge_text: Attribute.String;
+    cycle: Attribute.String;
+    stripe_data: Attribute.JSON;
+    price_data: Attribute.JSON;
+  };
+}
+
+export interface HeaderLink extends Schema.Component {
+  collectionName: 'components_header_links';
+  info: {
+    displayName: 'Link';
+    icon: 'link';
+    description: '';
+  };
+  attributes: {
+    label: Attribute.String;
+    description: Attribute.String;
+    to: Attribute.String;
+    icon: Attribute.String;
+    external: Attribute.Boolean;
+    target: Attribute.Enumeration<['_blank']>;
+    children: Attribute.JSON;
+  };
+}
+
+export interface FaqQuestion extends Schema.Component {
+  collectionName: 'components_faq_questions';
+  info: {
+    displayName: 'Question';
+    icon: 'question';
+  };
+  attributes: {
+    title: Attribute.String;
+    body: Attribute.RichText;
+  };
+}
+
+export interface FaqFaqSection extends Schema.Component {
+  collectionName: 'components_faq_faq_sections';
+  info: {
+    displayName: 'FAQ Section';
+    icon: 'question';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    questions: Attribute.Component<'faq.question', true>;
+  };
+}
+
+export interface DocumentationDocSection extends Schema.Component {
+  collectionName: 'components_documentation_doc_sections';
+  info: {
+    displayName: 'Doc Section';
+    icon: 'file';
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.RichText;
+    text_content: Attribute.RichText;
+    tabs: Attribute.Component<'tool.parameter', true>;
+    links: Attribute.Component<'header.link', true>;
+  };
+}
+
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'tool.parameter': ToolParameter;
+      'tool.card': ToolCard;
       'shared.string': SharedString;
       'shared.slider': SharedSlider;
       'shared.seo': SharedSeo;
@@ -449,23 +451,21 @@ declare module '@strapi/types' {
       'shared.cta': SharedCta;
       'shared.content-section': SharedContentSection;
       'shared.card': SharedCard;
-      'pricing.feature': PricingFeature;
-      'pricing.card': PricingCard;
       'product.tool-group': ProductToolGroup;
       'product.plugins-group': ProductPluginsGroup;
       'product.pdfassistant-product-section': ProductPdfassistantProductSection;
-      'header.link': HeaderLink;
-      'faq.question': FaqQuestion;
-      'faq.faq-section': FaqFaqSection;
-      'tool.parameter': ToolParameter;
-      'tool.card': ToolCard;
-      'documentation.doc-section': DocumentationDocSection;
       'pricing-section.pricing-section': PricingSectionPricingSection;
       'pricing-section.pricing-link': PricingSectionPricingLink;
       'pricing-section.pricing-features': PricingSectionPricingFeatures;
       'pricing-section.feature': PricingSectionFeature;
       'pricing-section.dynamic-cta': PricingSectionDynamicCta;
       'pricing-section.cta': PricingSectionCta;
+      'pricing.feature': PricingFeature;
+      'pricing.card': PricingCard;
+      'header.link': HeaderLink;
+      'faq.question': FaqQuestion;
+      'faq.faq-section': FaqFaqSection;
+      'documentation.doc-section': DocumentationDocSection;
     }
   }
 }
