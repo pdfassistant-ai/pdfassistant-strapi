f184b8d3738edfbeb4e788fea8a06730cd3b9394
datalogics-dliang <dliang@datalogics.com>
2026-06-22 14:07:38 -0500

    Add llms-txt content type

---
+++ src/api/llms-txt/controllers/llms-txt.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * llms-txt controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::llms-txt.llms-txt');
---
+++ src/api/llms-txt/routes/llms-txt.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * llms-txt router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::llms-txt.llms-txt');
